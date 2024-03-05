13291b9804d8943587d5e7c83e35939326285056
Grigori200 <mateusz.grzesiuk@op.pl>
2024-03-05 10:55:01 +0100

    Added scripts

--- scripts/run_jupyter.sh
+++ scripts/run_jupyter.sh
@@ -1,6 +1,6 @@
-#!bin/bash
+#!/bin/bash
 
 # usage in MAIN DIRECTORY USE THIS SCRIPT: bash scripts/run_jupyter.sh $port
 
 export PYTHONPATH="$PWD/source"
-jupyter lab --no-browser --port=$1
+jupyter lab --no-browser --port="$1"
